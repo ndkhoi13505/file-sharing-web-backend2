61699061f0bbe56b81eea104a666b903a905d139
khoidauminh <casboi86@gmail.com>
2025-11-18 10:58:10 +0700

    Use service instead

--- test/auth_test.go
+++ test/auth_test.go
@@ -4,40 +4,33 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/dath-251-thuanle/file-sharing-web-backend2/internal/domain"
 	"github.com/dath-251-thuanle/file-sharing-web-backend2/internal/infrastructure/database"
 	"github.com/dath-251-thuanle/file-sharing-web-backend2/internal/repository"
+	"github.com/dath-251-thuanle/file-sharing-web-backend2/internal/service"
 )
 
 func TestCreateUser(t *testing.T) {
 	name := "kdm"
 	password := "bitch133"
 	email := "kdm@gmail.com"
-	enableTOTP := false
+	role := "user"
 
 	database.InitDB()
 
 	userrepo := repository.NewSQLUserRepository(database.DB)
+	service := service.NewUserService(userrepo)
 
-	user := domain.User{
-		Username:   name,
-		Password:   password,
-		Email:      email,
-		EnableTOTP: enableTOTP,
-	}
-	err := userrepo.Create(&user)
-	if err != nil {
+	if err := service.CreateUser(name, password, email, role); err != nil {
 		t.Errorf(`FAILED: %v`, err)
 		return
 	}
 
-	founduser := domain.User{}
-	err = userrepo.FindByEmail(email, &founduser)
+	user, err := service.GetUserByEmail(email)
 
 	if err != nil {
 		t.Errorf(`FAILED: %v`, err)
 		return
 	}
 
-	fmt.Print(founduser)
+	fmt.Println(user)
 }
--- test/file_test.go
+++ test/file_test.go
@@ -1,1 +1,8 @@
 package test
+
+import (
+	"testing"
+)
+
+func TestFileUpload(t *testing.T) {
+}
